b21213a288c1111524ffd5f7d65b6ca80b3578f4
Will Papper <contact@papper.me>
2021-09-01 07:24:09 -0700

    Added deployment code

--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,9 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require("dotenv").config();
+const { MNEMONIC, INFURA_API_KEY } = process.env;
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
@@ -42,36 +44,33 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+      websockets: true, // Enable EventEmitter interface for web3 (default: false)
+    },
+    rinkeby: {
+      provider: new HDWalletProvider({
+        mnemonic: MNEMONIC,
+        providerOrUrl: `wss://rinkeby.infura.io/ws/v3/${INFURA_API_KEY}`,
+        numberOfAddresses: "1",
+        derivationPath: "m/44'/60'/0'/0/0",
+      }),
+      network_id: 4,
+      websockets: true,
+    },
+    mainnet: {
+      provider: new HDWalletProvider({
+        mnemonic: MNEMONIC,
+        providerOrUrl: `wss://mainnet.infura.io/ws/v3/${INFURA_API_KEY}`,
+        numberOfAddresses: "1",
+        derivationPath: "m/44'/60'/0'/0/0",
+      }),
+      network_id: 1,
+      websockets: true,
+      gasPrice: 99000000000, // 99 gwei (99 * 10e9) wei
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -82,16 +81,16 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "^0.8.7", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
-    }
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+        //  evmVersion: "byzantium"
+      },
+    },
   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
