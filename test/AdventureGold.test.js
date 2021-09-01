21284255bac45bf685f8d7dde96f5ffdc0f0b459
Will Papper <contact@papper.me>
2021-09-01 10:30:29 -0700

    Set up Loot ABI

--- test/AdventureGold.test.js
+++ test/AdventureGold.test.js
@@ -4,12 +4,14 @@ const { expect } = require("chai");
 require("chai").use(require("chai-as-promised")).should();
 
 const AdventureGold = artifacts.require("AdventureGold");
-const Loot = require("./abi/Loot");
+const LootABI = require("./abi/Loot").abi;
+const lootContract = new web3.eth.Contract(LootABI, "0xFF9C1b15B16263C61d017ee9F65C50e4AE0113D7");
 
 contract('AdventureGold', (accounts) => {
   it('Contract names should be correct', async () => {
     const adventureGoldInstance = await AdventureGold.deployed();
 
     assert.equal(await adventureGoldInstance.name(), 'Adventure Gold');
+    assert.equal(await lootContract.methods.name().call(), 'Loot');
   });
 });
